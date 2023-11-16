5ccf45188c89ffbc19af47057062c3d4bf498738
Navusas <D.Giedraitis7@gmail.com>
2023-11-16 13:43:49 +0000

    Refactor the code to create a separate class for handling database requests and instantiate it in the main function.

--- postgres_da_ai_agent/main.py
+++ postgres_da_ai_agent/main.py
@@ -3,11 +3,9 @@ from flask import Flask, jsonify, request
 import threading
 import dotenv
 import argparse
-# Import other necessary modules
 
 dotenv.load_dotenv()
 
-# Flask app setup
 app = Flask(__name__)
 
 # Global variable to store task status
@@ -30,21 +28,21 @@ def background_task(prompt):
     task_status["success"] = True
     task_status["message"] = "Task completed successfully"
 
-@app.route('/get-sql-query', methods=['POST'])
 class DatabaseRequest:
     def __init__(self, data):
         self.database = data['database']
         self.request = data['request']
 
+@app.route('/get-sql-query', methods=['POST'])
 def get_sql_query():
     api_key = request.headers.get('api-key')
     if not api_key:
         return jsonify({"message": "No API key provided"}), 400
     
     print(request)
-    
+
     data = request.json
-    db_request = DatabaseRequest(data)
+    db_request = DatabaseRequest(data=data)
 
     if not db_request.request.get("Prompt"):
         return jsonify({"message": "No prompt provided"}), 400
@@ -62,6 +60,7 @@ def status():
 
 def main():
     app.run(debug=True)
+
 
 if __name__ == "__main__":
     main()
