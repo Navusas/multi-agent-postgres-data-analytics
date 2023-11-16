7e13514c1031a68915aa8e5f8182e29c5c50aec5
Navusas <D.Giedraitis7@gmail.com>
2023-11-16 13:27:55 +0000

    Refactor main.py to use Flask and handle background tasks.

--- postgres_da_ai_agent/main.py
+++ postgres_da_ai_agent/main.py
@@ -1,103 +1,53 @@
 import os
-from postgres_da_ai_agent.modules.db import PostgresManager
-from postgres_da_ai_agent.modules import llm
-from postgres_da_ai_agent.modules import orchestrator
-from postgres_da_ai_agent.modules import file
-from postgres_da_ai_agent.modules import embeddings
-from postgres_da_ai_agent.agents import agents
+from flask import Flask, jsonify, request
+import threading
 import dotenv
 import argparse
-import autogen
+# Import other necessary modules
 
 dotenv.load_dotenv()
 
-assert os.environ.get("DATABASE_URL"), "POSTGRES_CONNECTION_URL not found in .env file"
-assert os.environ.get(
-    "OPENAI_API_KEY"
-), "POSTGRES_CONNECTION_URL not found in .env file"
+# Flask app setup
+app = Flask(__name__)
 
-DB_URL = os.environ.get("DATABASE_URL")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+# Global variable to store task status
+task_status = {"running": False, "success": False, "message": ""}
 
-POSTGRES_TABLE_DEFINITIONS_CAP_REF = "TABLE_DEFINITIONS"
-RESPONSE_FORMAT_CAP_REF = "RESPONSE_FORMAT"
-SQL_DELIMITER = "---------"
+def background_task(prompt):
+    """
+    The function to handle the background task.
+    This is where you'll incorporate your existing logic.
+    """
+    global task_status
+    task_status["running"] = True
+    task_status["success"] = False
 
+    # Your task logic goes here
+    # For example, handling the prompt with your existing code
 
-import argparse
-import os
+    # Update task status upon completion
+    task_status["running"] = False
+    task_status["success"] = True
+    task_status["message"] = "Task completed successfully"
 
-def main():
-    parser = argparse.ArgumentParser(description='Process command line arguments.')
-    parser.add_argument('--prompt', type=str, help='Prompt for the AI')
-    parser.add_argument('--connection-string', type=str, help='Database connection string')
-    parser.add_argument('--openai-api-key', type=str, help='OpenAI API key')
+@app.route('/get-sql-query', methods=['POST'])
+def get_sql_query():
+    data = request.json
+    prompt = data.get("prompt", "")
 
-    args = parser.parse_args()
+    if not prompt:
+        return jsonify({"message": "No prompt provided"}), 400
 
-    DB_URL = os.getenv('DB_URL')
-    OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+    # Start the background task
+    thread = threading.Thread(target=background_task, args=(prompt,))
+    thread.start()
 
-    if DB_URL is None:
-        if args.connection_string is not None:
-            DB_URL = args.connection_string
-        else:
-            raise ValueError('DB_URL is not set. Please set the DB_URL environment variable or use the --connection-string flag.')
+    return jsonify({"message": "Task started", "status": "running"}), 202
 
-    if OPENAI_API_KEY is None:
-        if args.openai_api_key is not None:
-            OPENAI_API_KEY = args.openai_api_key
-        else:
-            raise ValueError('OPENAI_API_KEY is not set. Please set the OPENAI_API_KEY environment variable or use the --openai-api-key flag.')
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--prompt", help="The prompt for the AI")
-    args = parser.parse_args()
-
-    if not args.prompt:
-        print("Please provide a prompt")
-        return
-
-    raw_prompt = args.prompt
-
-    prompt = f"Fulfill this database query: {raw_prompt}. "
-
-    with PostgresManager() as db:
-        db.connect_with_url(DB_URL)
-
-        map_table_name_to_table_def = db.get_table_definition_map_for_embeddings()
-
-        database_embedder = embeddings.DatabaseEmbedder()
-
-        for name, table_def in map_table_name_to_table_def.items():
-            database_embedder.add_table(name, table_def)
-
-        similar_tables = database_embedder.get_similar_tables(raw_prompt, n=5)
-
-        table_definitions = database_embedder.get_table_definitions_from_names(
-            similar_tables
-        )
-
-        prompt = llm.add_cap_ref(
-            prompt,
-            f"Use these {POSTGRES_TABLE_DEFINITIONS_CAP_REF} to satisfy the database query.",
-            POSTGRES_TABLE_DEFINITIONS_CAP_REF,
-            table_definitions,
-        )
-
-        data_eng_orchestrator = agents.build_team_orchestrator("data_eng", db)
-
-        success, data_eng_messages = data_eng_orchestrator.sequential_conversation(
-            prompt
-        )
-
-        # ---------------------------------------------
-
-        data_eng_cost, data_eng_tokens = data_eng_orchestrator.get_cost_and_tokens()
-
-        print(f"Data Eng Cost: {data_eng_cost}, tokens: {data_eng_tokens}")
-
-        print(f"💰📊🤖 Organization Cost: {data_eng_cost}, tokens: {data_eng_tokens}")
+@app.route('/status', methods=['GET'])
+def status():
+    return jsonify(task_status)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
